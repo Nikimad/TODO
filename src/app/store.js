7219e8aeae64c87e2ddcc982e2f38e148584c979
Nikimad <PybePoiD@gmail.com>
2023-02-23 21:05:00 +0300

    Add theme, list reducers and localStorageMW to store:

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import theme from "../features/theme/themeSlice";
+import list from "../features/list/listSlice";
+import localStorageMiddleware from "../middlewares/localStorageMiddlware";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    theme,
+    list,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
