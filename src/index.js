7f71d064d94275c8af08c3637dd86aa6a6e73fff
Nikimad <PybePoiD@gmail.com>
2023-02-23 21:05:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import App from "./components/App";
 import "./styles/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
