0d9541213bb1194400d000c799c8cee3060d661f
Nikimad <PybePoiD@gmail.com>
2023-02-24 19:54:07 +0300

    Update App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,13 @@
 import Wrapper from "../Wrapper";
 import Header from "../Header";
+import Main from "../Main";
+import Footer from "../Footer";
 
 const App = () => (
   <Wrapper>
     <Header />
+    <Main />
+    <Footer />
   </Wrapper>
 );
 
