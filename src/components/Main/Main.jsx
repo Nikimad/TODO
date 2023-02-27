8a117f0a8a205317ea4c8375c1686ace1cae731f
Nikimad <PybePoiD@gmail.com>
2023-02-27 17:35:08 +0300

    Refact Main component: add empty state

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,26 @@
 import "./Main.css";
 import Item from "../Item";
 
-const Main = ({ items, onDeleteCompleted, isDisabled, activeCount }) => (
+const Main = ({
+  items,
+  onDeleteCompleted,
+  isDisabled,
+  activeCount,
+  filter,
+  isEmpty,
+}) => (
   <main className="container todo-app__main">
-    <ul className="todo-app__list">
-      {items.map((item) => (
-        <Item key={item.id} item={item} />
-      ))}
-    </ul>
+    {isEmpty ? (
+      <p className="todo-app__list-plug">
+        No {filter === "all" ? "" : filter} tasks
+      </p>
+    ) : (
+      <ul className="todo-app__list">
+        {items.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    )}
     <div className="todo-app__main-controls">
       <p className="todo-app__counter">
         {activeCount} {activeCount === 1 ? "Item left" : "Items left"}
--- src/components/Main/MainContainer.jsx
+++ src/components/Main/MainContainer.jsx
@@ -3,12 +3,16 @@ import { deleteCompleted } from "../../features/list/listSlice";
 import {
   selectActiveCounter,
   selectCompletedCounter,
+  selectFilter,
   selectVisibleItems,
+  selectVisibleItemsCounter,
 } from "../../features/list/selectors/selectors";
 import Main from "./Main";
 
 const MainContainer = () => {
+  const currentFilter = useSelector(selectFilter);
   const items = useSelector(selectVisibleItems);
+  const visibleCount = useSelector(selectVisibleItemsCounter)
   const activeCount = useSelector(selectActiveCounter);
   const completedCount = useSelector(selectCompletedCounter);
 
@@ -17,7 +21,9 @@ const MainContainer = () => {
 
   return (
     <Main
+      filter={currentFilter}
       items={items}
+      isEmpty={visibleCount === 0}
       activeCount={activeCount}
       onDeleteCompleted={handleDeleteCompleted}
       isDisabled={completedCount === 0}
