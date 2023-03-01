89ef1800499536d6df64c95fcd537c4c39051bb6
Nikimad <PybePoiD@gmail.com>
2023-03-01 13:01:17 +0300

    Add pages/HomePage component

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import Wrapper from "../../components/Wrapper";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import Footer from "../../components/Footer";
+
+const HomePage = () => (
+  <Wrapper>
+    <Header />
+    <Main />
+    <Footer />
+  </Wrapper>
+);
+
+export default HomePage;
