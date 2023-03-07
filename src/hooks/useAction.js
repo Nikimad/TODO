bdcfc93415f83ae7e8d2f5a0f36cb4dc2431ebb4
Nikimad <PybePoiD@gmail.com>
2023-03-07 22:29:13 +0300

    Add useAction hook

---
+++ src/hooks/useAction.js
@@ -0,0 +1,8 @@
+import { useDispatch } from "react-redux";
+
+const useAction = (action) => {
+    const dispatch = useDispatch();
+    return () => dispatch(action);
+};
+
+export default useAction;
